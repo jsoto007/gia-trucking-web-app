8a813d80ccad6615eef9b8f82e01bb84d9510bfb
Jose Soto <jsoto7087@gmail.com>
2024-03-01 16:29:04 -0500

    removes menu features

--- src/app/home/Hero.js
+++ src/app/home/Hero.js
@@ -6,6 +6,7 @@ import trucks from "../../../public/trucks.jpg"
 import giaBlueTruck from "../../../public/giaBlueTruck.jpg"
 import giaGreenTruck from "../../../public/giaGreenTruck.jpg"
 import Image from 'next/image'
+import NavBar from './NavBar'
 
 const navigation = [
   { name: 'Product', href: '#' },
@@ -19,50 +20,12 @@ export default function Example() {
 
   return (
     <div className="bg-white">
+      <NavBar />
       <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-          <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
-            </a>
-          </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          {/* Navigation  */}
-          <div className="hidden lg:flex lg:gap-x-12 md:mr-20">
-            {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
-                {item.name}
-              </a>
-            ))}
-          </div>
-        
-        </nav>
         <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
-                />
-              </a>
               <button
                 type="button"
                 className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -74,17 +37,6 @@ export default function Example() {
             </div>
             <div className="mt-6 flow-root">
               <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
                 <div className="py-6">
                   <a
                     href="#"
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@ import Hero from "../app/home/Hero"
 
 export default function Home() {
   return (
-   <Hero />
+    <div>
+      <Hero />
+    </div>
   );
 }
