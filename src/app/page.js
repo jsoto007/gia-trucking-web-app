dbf7d5f38c81bc964283a23c71aeb9c02c71ba93
Jose Soto <jsoto7087@gmail.com>
2024-03-03 23:51:11 -0500

    final update for presentation

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,6 @@ import About from "./about/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
-
-
 export default function Home() {
   return (
     <div className="bg-white">
