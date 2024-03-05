ee5adaac6f8a4d55518da77715e9c4a8d19aafb4
Jose Soto <jsoto7087@gmail.com>
2024-03-04 22:24:28 -0500

    added mission statement

--- src/app/about/About.js
+++ src/app/about/About.js
@@ -9,21 +9,14 @@ export default function About() {
         <div className="mt-6 flex flex-col gap-x-8 gap-y-20 lg:flex-row">
           <div className="lg:w-full lg:max-w-2xl lg:flex-auto">
             <p className="text-xl leading-8 text-gray-600">
-              Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam
-              eget aliquam. Quisque id at vitae feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget.
-              Eleifend egestas fringilla sapien.
+            At GIA Blessed Trucking, we are committed to delivering outstanding logistical services to our clients with honesty, dependability, and effectiveness. Our focus is on ensuring safe, timely, and cost-effective delivery of every shipment while continuously innovating and adapting to meet our customers' evolving needs.
             </p>
             <div className="mt-10 max-w-xl text-base leading-7 text-gray-700">
               <p>
-                Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed
-                amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius
-                sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim.
-                Mattis mauris semper sed amet vitae sed turpis id.
+              Our aim is to exceed client expectations by delivering unmatched logistical solutions. We create long-lasting relationships based on trust with personalized services and innovative solutions. Our team of skilled professionals is committed to surpassing client requirements and going above and beyond to provide the best possible experience.
               </p>
               <p className="mt-10">
-                Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie
-                auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et
-                ultrices hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.
+              Our values of integrity, reliability, and excellence guide us as we pursue our mission. We strive for quality and customer satisfaction to set the standard in transportation and build lasting trust with our clients.
               </p>
             </div>
           </div>
