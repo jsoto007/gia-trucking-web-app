d615c14d2ade6d085ba26631c871cdd46d3a5602
Jose Soto <jsoto7087@gmail.com>
2024-03-08 23:27:07 -0500

    working with no errors

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -78,7 +78,7 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-center text-xs leading-5 text-gray-500">
-            &copy; 2024 GIA BLESSED TRUCKING, Inc. All rights reserved.
+            &copy; 2024 GIA BLESSED TRUCKING, Inc. All rights reserved. <span className="text-xs ml-10">Powered by J Soto</span>
           </p>
         </div>
       </div>
