035604d3403211a702208ee5e8baa2676f60730d
Jose Soto <jsoto7087@gmail.com>
2024-03-16 23:54:03 -0400

    error: 400 invalid public key resolved. User can email from app

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -1,17 +1,19 @@
 'use client'
 import React, { useRef } from "react";
-import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
 
 export default function Contact() {
 
   const form = useRef();
 
-  function sendEmail(e) {
+
+
+  const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs.sendForm('service_xtdhy8f', 'template_6qt1izy', e.target, {
-        publicKey: 'wlJlBI_q6JBqv5Lx_',
+        publicKey: '_cP9azToazeRYCd3T',
       })
       .then(
         () => {
@@ -21,7 +23,9 @@ export default function Contact() {
           console.log('FAILED...', error.text);
         },
       );
-  }
+  };
+
+  
   return (
     <div className="relative isolate bg-white" id="contact">
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
@@ -81,7 +85,8 @@ export default function Contact() {
             </dl>
           </div>
         </div>
-        <form action="#" method="POST" className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48" onSubmit={sendEmail}>
+
+        <form method="POST" className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48" onSubmit={sendEmail} href={form}>
           <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
          
@@ -114,7 +119,7 @@ export default function Contact() {
                 </div>
               </div>
               <div className="sm:col-span-2">
-                {/* <label htmlFor="text" className="block text-sm font-semibold leading-6 text-gray-900">
+                <label htmlFor="text" className="block text-sm font-semibold leading-6 text-gray-900">
                   Phone number
                 </label>
                 <div className="mt-2.5">
@@ -126,7 +131,7 @@ export default function Contact() {
                     autoComplete="tel"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
-                </div> */}
+                </div>
               </div>
               <div className="sm:col-span-2">
                 <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
@@ -153,6 +158,7 @@ export default function Contact() {
             </div>
           </div>
         </form>
+        
       </div>
     </div>
   )
