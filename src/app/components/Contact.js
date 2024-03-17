1fc537f2316e76950726d16bc5c1005f170a20fd
Jose Soto <jsoto7087@gmail.com>
2024-03-16 23:23:46 -0400

    working on publick key error

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -1,7 +1,27 @@
-import React from "react";
+'use client'
+import React, { useRef } from "react";
+import emailjs from 'emailjs-com';
 import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
 
 export default function Contact() {
+
+  const form = useRef();
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_xtdhy8f', 'template_6qt1izy', e.target, {
+        publicKey: 'wlJlBI_q6JBqv5Lx_',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  }
   return (
     <div className="relative isolate bg-white" id="contact">
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
@@ -61,33 +81,20 @@ export default function Contact() {
             </dl>
           </div>
         </div>
-        <form action="#" method="POST" className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
+        <form action="#" method="POST" className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48" onSubmit={sendEmail}>
           <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-              <div>
-                <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
-                  First name
+         
+              <div className="sm:col-span-2">
+                <label htmlFor="subject" className="block text-sm font-semibold leading-6 text-gray-900">
+                  Subject
                 </label>
                 <div className="mt-2.5">
                   <input
-                    type="text"
-                    name="first-name"
-                    id="first-name"
-                    autoComplete="given-name"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <div>
-                <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
-                  Last name
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    type="text"
-                    name="last-name"
-                    id="last-name"
-                    autoComplete="family-name"
+                    type="subject"
+                    name="subject"
+                    id="subject"
+                    autoComplete="subject"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -107,18 +114,19 @@ export default function Contact() {
                 </div>
               </div>
               <div className="sm:col-span-2">
-                <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
+                {/* <label htmlFor="text" className="block text-sm font-semibold leading-6 text-gray-900">
                   Phone number
                 </label>
                 <div className="mt-2.5">
                   <input
-                    type="tel"
-                    name="phone-number"
-                    id="phone-number"
+                    type="text"
+                    href="current"
+                    name="phone_number"
+                    id="phone_number"
                     autoComplete="tel"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
-                </div>
+                </div> */}
               </div>
               <div className="sm:col-span-2">
                 <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
